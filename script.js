fb1e2e307df5e0aafaf07cc1bf7a787010f1d18e
jafardahod <jafardahod7@gmail.com>
2024-08-12 20:42:31 +0530

    changed the js code

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ let trigger = true
 btn.addEventListener('click', (e) => {
     if(trigger){
         if(e.target){
-            document.body.style.background = 'white'
-            document.body.style.color = 'black'
+            document.body.style.background = 'black'
+            document.body.style.color = 'white'
+            btn.innerHTML = 'Light Mode'
 
             trigger = false
         }
     }else{
         if(e.target){
-            document.body.style.background = 'black'
-            document.body.style.color = 'white'
+            document.body.style.background = 'white'
+            document.body.style.color = 'black'
+            btn.innerHTML = 'Dark Mode'
             trigger = true
 
         }
